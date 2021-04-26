b6b0c6b35e9638081910af6d299469a33d25c41e
Rüveyda Dal <71987230+ruv19@users.noreply.github.com>
2021-04-26 16:25:41 +0300

    Add files via upload

---
+++ Kendisine En Yakin Asal Sayiyi Veren C Programi.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (void)
+{
+	int num,i,j,k;
+	printf("\n bir sayi giriniz:");
+	scanf("%d",&num);
+	for(i=2;i<num;i++)
+	{
+		if(num%i==0)
+		{
+			break;
+		}
+	}
+	
+	
+	j=num;
+	for(k=2;k<j;k++)
+	{
+		if(j%k==0)
+		{
+			k=2;
+			j--;
+		}
+	}
+	if(j==num)
+	{
+		printf("\n sayi asaldir.");
+	}
+	
+	else
+	{
+		printf("\n sayi asal degildir, sayiya en yakin asal sayi  %d",j);
+	}
+	
+	
+	
+	
+	
+}
