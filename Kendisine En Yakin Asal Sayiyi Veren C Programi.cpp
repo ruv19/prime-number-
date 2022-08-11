5a74e1790aab19a983ef438b207d2c96edd0a680
Rüveyda Dal <ruveydadal.3@gmail.com>
2022-08-11 16:42:37 +0300

    Update Kendisine En Yakin Asal Sayiyi Veren C Programi.cpp

--- Kendisine En Yakin Asal Sayiyi Veren C Programi.cpp
+++ Kendisine En Yakin Asal Sayiyi Veren C Programi.cpp
@@ -1,41 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main (void)
-{
+int main (void) {
+
 	int num,i,j,k;
 	printf("\n bir sayi giriniz:");
 	scanf("%d",&num);
-	for(i=2;i<num;i++)
-	{
-		if(num%i==0)
-		{
+	for(i=2; i<num; i++) {
+		if(num%i == 0) {
 			break;
 		}
 	}
 	
-	
 	j=num;
-	for(k=2;k<j;k++)
-	{
-		if(j%k==0)
-		{
-			k=2;
+	for(k=2; k<j ;k++) {
+		if(j%k == 0) {
+			k = 2;
 			j--;
 		}
 	}
-	if(j==num)
-	{
+	if(j == num) {
+
 		printf("\n sayi asaldir.");
-	}
-	
-	else
-	{
+	} else {
 		printf("\n sayi asal degildir, sayiya en yakin asal sayi  %d",j);
 	}
-	
-	
-	
-	
-	
 }
